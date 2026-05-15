ad0b3e9a462e3cd3ded7ee4351d102e202a893c7
lehonglinh12345 <lehonglinhcd2004@gmail.com>
2026-05-15 19:58:19 +0700

    cập nhật link tyb

--- src/components/ProjectShowcase.tsx
+++ src/components/ProjectShowcase.tsx
@@ -30,7 +30,7 @@ const ProjectShowcase = memo(function ProjectShowcase() {
       mainImage: "/images/input_file_1.png",
       gallery: [],
       color: "bg-studio-red",
-      videoUrl: "https://www.youtube.com/embed/TM142-7LiiQ",
+      videoUrl: "https://youtu.be/bB6ZTFAWmcQ",
       tags: ["3D Animation", "Creative Direction", "CGI", "Visual Storytelling"]
     }
   ];
