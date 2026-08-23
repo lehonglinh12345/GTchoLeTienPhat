327f8578071bb5c0c18946f9f0793d5fbc2fcfd3
lehonglinh12345 <lehonglinhcd2004@gmail.com>
2026-08-23 16:54:06 +0700

    Add Vercel configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -55,6 +55,10 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production' && process.env.VERCEL !== '1') {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+export default app;
